d7ee68e1fa4f4aaf99d76c3701760ef8101c334f
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2024-04-24 16:31:20 +0530

    completed the code to display the data from serial port in the console

--- UI/serial_data_receiver/serial_data_receiver/Program.cs
+++ UI/serial_data_receiver/serial_data_receiver/Program.cs
@@ -3,8 +3,25 @@ using System.IO.Ports;
 
 class DataReceiver
 {
-    static void main (String[] args)
+    static void Main (String[] args)
     {
         SerialPort mySerialPort = new SerialPort("COM3", 115200);
+        try
+        {
+            mySerialPort.Open();
+            mySerialPort.DataReceived += (sender, e) =>
+            {
+                string data = mySerialPort.ReadExisting();
+                Console.Write(data);
+            };
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error : {ex.Message}");
+        }
+        finally {
+            mySerialPort.Close(); 
+        }
     }
 }
