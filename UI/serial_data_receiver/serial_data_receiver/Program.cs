84d6fa9fc953fbc9c16514fecd44620f2c535417
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2024-04-24 16:15:43 +0530

    started working on the receiver

---
+++ UI/serial_data_receiver/serial_data_receiver/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO.Ports;
+
+class DataReceiver
+{
+    static void main (String[] args)
+    {
+        SerialPort mySerialPort = new SerialPort("COM3", 115200);
+    }
+}
